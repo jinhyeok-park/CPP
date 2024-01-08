012c36f84690e215f568f147de2c67c0c4dfb7c7
Jinhyeok Park <jinhyeok@c4r5s6.42seoul.kr>
2024-01-08 18:37:55 +0900

    type cpp05 body makefile

---
+++ 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,18 @@
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat {
+
+private :
+    const std::string name = "default";
+    int               grade;
+
+public :
+
+
+};
+
+#endif
